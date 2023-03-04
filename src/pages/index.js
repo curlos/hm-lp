42ec146b4efc15bb0b3abcaa7e879c0803cfdb5f
Carlos Martinez <curlosmart@gmail.com>
2023-03-04 04:46:36 -0500

    fixed home error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
@@ -11,21 +11,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main>
 
         Hello World
 
       </main>
     </>
-  )
+  );
 }
 
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
 export async function getStaticProps() {
-  redirect('/savings');
 
   return {
     // Next.js will attempt to re-generate the page:
@@ -33,5 +32,5 @@ export async function getStaticProps() {
     // - At most once every 10 seconds
     props: {},
     revalidate: 10, // In seconds
-  }
+  };
 }
